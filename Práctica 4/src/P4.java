fda4206cb15dbc731b13471c838f0fef42edaf64
Rubén García <rubentxogg@hotmail.com>
2021-02-01 14:11:20 +0100

    [ADD] Práctica 4

---
+++ Práctica 4/src/P4.java
@@ -0,0 +1,14 @@
+/**
+ * @author Rubén García García
+ * 
+ * La práctica tendrá que realizar las siguientes tareas:
+ * 
+ * Crear dos métodos que calculen el mayor número introducido y el menor número introducido.
+ * Sacar los resultados por pantalla.
+ */
+public class P4 {
+	
+	public static void main(String[] args) {
+		new Numbers().solicNum(); // Creando el método de la clase Numbers
+	}
+}
