407323941e09852b4ae66c19b330ff27e95d09dc
Rubén García <rubentxogg@hotmail.com>
2021-02-02 18:52:00 +0100

    [MOD] Práctica 7 terminada

--- Práctica_7/src/Number.java
+++ Práctica_7/src/Number.java
@@ -18,9 +18,10 @@ public class Number {
 		int opc = 0;
 		
 		do {
-			System.out.println("¿Qué tipo de array desea crear?"
+			System.out.print("¿Qué tipo de array desea crear?"
 					+"\n1.- Unidimensional"
-					+"\n2.- Bidimensional");
+					+"\n2.- Bidimensional"
+					+"\nSeleccione una opción: ");
 			opc = sc.nextInt();
 			
 			switch(opc) {
@@ -30,8 +31,10 @@ public class Number {
 				case 2: arrayBidimensional();
 				break;
 				
-				default: System.out.println("Ha seleccionado una opción inválida, vuelva a intentarlo.");
-				System.out.println("--------------------------------------");
+				default: System.out.println("-----------------------------------------------------------------");
+						 System.out.println("Ha seleccionado una opción inválida, vuelva a intentarlo.");
+						 System.out.println("-----------------------------------------------------------------");
+						 break;
 			}
 			
 			
@@ -47,7 +50,7 @@ public class Number {
 		Scanner sc = new Scanner(System.in);
 		int filas, columnas;
 		
-		System.out.println("--------------------------------------");
+		System.out.println("-----------------------------------------------------------------");
 		System.out.print("Introduzca la cantidad de filas que desea introducir: ");
 		filas = sc.nextInt();
 	
@@ -55,37 +58,42 @@ public class Number {
 		columnas = sc.nextInt();
 		arrayBiNum = new int[filas][columnas];
 		
-		Arrays.fill(arrayBiNum, Math.random()*arrayBiNum.length);
+		
+		for(int i=0;i<arrayBiNum.length;i++) {
+			for(int j=0;j<arrayBiNum[i].length;j++) {
+				arrayBiNum[i][j] = (int) (Math.random()* 10);
+			}	
+		}
+		
+		System.out.println("-----------------------------------------------------------------"
+				+ "\nArray bidimensional:");
+		for(int[] x: arrayBiNum) {
+			System.out.println();
+			for(int num: x) {
+				System.out.print(num+" ");
+			}
+		}
+		System.out.println();
+		System.out.println("-----------------------------------------------------------------");
+		
+		mediaNum(arrayBiNum);
+		sumaNum(arrayBiNum);
+		productNum(arrayBiNum);
+		mayorNum(arrayBiNum);
+		menorNum(arrayBiNum);
+		positivNum(arrayBiNum);
+		sc.close();
 	}
-//		for(int i=0;i<arrayBiNum.length;i++) {
-//			for(int j=0;j<arrayBiNum[i].length;j++) {
-//				arrayBiNum[i][j] = (int) (Math.random()*arrayBiNum.length);
-//			}
-//			
-//		}
-//		
-//		System.out.println("--------------------------------------");
-//		System.out.println(Arrays.toString(arrayBiNum));
-//		System.out.println("--------------------------------------");
-//		
-//		mediaNum();
-//		sumaNum();
-//		productNum();
-//		mayorNum();
-//		menorNum();
-//		positivNum();
-//		sc.close();
-//	}
-//	
+	
 	
 	/**
-	 * Método que crea un array unidimensional
+	 * Método que crea un array unidimensional y lo rellena de números pasados por el usuario
 	 */
 	public void arrayUnidimensional() {
 		Scanner sc = new Scanner(System.in);
 		int cantidad;
 		
-		System.out.println("--------------------------------------");
+		System.out.println("-----------------------------------------------------------------");
 		System.out.print("Introduzca la cantidad de números que desea introducir: ");
 		cantidad = sc.nextInt();
 		arrayUniNum = new int[cantidad];
@@ -95,23 +103,24 @@ public class Number {
 			arrayUniNum[i] = sc.nextInt();
 		}
 		
-		System.out.println("--------------------------------------");
+		System.out.println("-----------------------------------------------------------------"
+				+ "\nArray unidimensional:\n");
 		System.out.println(Arrays.toString(arrayUniNum));
-		System.out.println("--------------------------------------");
+		System.out.println("-----------------------------------------------------------------");
 		
-		mediaNum();
-		sumaNum();
-		productNum();
-		mayorNum();
-		menorNum();
-		positivNum();
+		mediaNum(arrayUniNum);
+		sumaNum(arrayUniNum);
+		productNum(arrayUniNum);
+		mayorNum(arrayUniNum);
+		menorNum(arrayUniNum);
+		positivNum(arrayUniNum);
 		sc.close();
 	}
 	
 	/**
-	 * Método que halla la media de los números introducidos por el usuario
+	 * Método que halla la media de los números del array unidimensional
 	 */
-	public void mediaNum() {
+	public void mediaNum(int[] arrayUniNum) {
 		int media = 0;
 		for(int i=0;i<arrayUniNum.length;i++) {
 			media += arrayUniNum[i];
@@ -121,66 +130,120 @@ public class Number {
 	}
 	
 	/**
-	 * Método que halla la suma de los números introducidos por el usuario
+	 * Método que halla la media de los números del array bidimensional
 	 */
-	public void sumaNum() {
+	public void mediaNum(int[][] arrayBiNum) {
+		int media = 0, tot = 0;
+		for(int i=0;i<arrayBiNum.length;i++) {
+			for(int j=0;j<arrayBiNum[i].length;j++,tot++)
+				media += arrayBiNum[i][j];
+		}
+		media = media / tot;
+		System.out.println("La media es: "+media);
+	}
+	
+	
+	/**
+	 * Método que halla la suma de los números del array unidimensional
+	 */
+	public void sumaNum(int[] arrayUniNum) {
 		int suma = 0;
 		for(int i=0;i<arrayUniNum.length;i++) {
 			suma += arrayUniNum[i];
 		}
 		System.out.println("La suma es: "+suma);
 	}
+	
+	/**
+	 * Método que halla la suma de los números del array bidimensional
+	 */
+	public void sumaNum(int[][] arrayBiNum) {
+		int suma = 0;
+		for(int i=0;i<arrayBiNum.length;i++) {
+			for(int j=0;j<arrayBiNum[i].length;j++)
+			suma += arrayBiNum[i][j];
+		}
+		System.out.println("La suma es: "+suma);
+	}
 
 
 	/**
-	 * Método que halla el producto de los números introducidos por el usuario
+	 * Método que halla el producto de los números del array unidimensional
 	 */
-	public void productNum() {
+	public void productNum(int[] arrayUniNum) {
 		int producto = 1;
 		for(int i=0;i<arrayUniNum.length;i++) {
 			producto = producto * arrayUniNum[i];
 		}
 		System.out.println("El producto es: "+producto);
-		System.out.println("--------------------------------------");
+		System.out.println("-----------------------------------------------------------------");
 	}
 	
 	/**
-	 * Método que calcula el mayor número introducido
+	 * Método que halla el producto de los números del array bidimensional
 	 */
-	public void mayorNum() {
+	public void productNum(int[][] arrayBiNum) {
+		int producto = 1;
+		for(int i=0;i<arrayBiNum.length;i++) {
+			for(int j=0;j<arrayBiNum[i].length;j++)
+			producto = producto * arrayBiNum[i][j];
+		}
+		System.out.println("El producto es: "+producto);
+		System.out.println("-----------------------------------------------------------------");
+	}
+	
+	/**
+	 * Método que calcula el mayor número del array unidimensional
+	 */
+	public void mayorNum(int[] arrayUniNum) {
 		Arrays.sort(arrayUniNum);
 		System.out.println("El mayor número es: "+arrayUniNum[arrayUniNum.length-1]);
-//		int mayor = 0;
-//		for(int i=0;i<arrayNum.length;i++) {
-//			if(arrayNum[i] > mayor) {
-//				mayor = arrayNum[i];
-//			}
-//		}
-//		System.out.println("El mayor número es: "+mayor);
 	}
 	
 	/**
-	 * Método que calcula el menor número introducido
+	 * Método que calcula el mayor número del array bidimensional
 	 */
-	public void menorNum() {
+	public void mayorNum(int[][] arrayBiNum) {
+		int numMay = 0;
+		for(int i=0;i<arrayBiNum.length;i++) {
+			for(int j=0;j<arrayBiNum[i].length;j++) {
+				if(numMay < arrayBiNum[i][j]) {
+					numMay = arrayBiNum[i][j];
+				}
+			}	
+		}
+		System.out.println("El mayor número es: "+numMay);
+	}
+	
+	/**
+	 * Método que calcula el menor número del array unidimensional
+	 */
+	public void menorNum(int[] arrayUniNum) {
 		Arrays.sort(arrayUniNum);
 		System.out.println("El menor número es: "+arrayUniNum[0]);
-		System.out.println("--------------------------------------");
-		
-//		int menor = 0;
-//		for(int i=0;i<arrayNum.length;i++) {
-//			if(menor < arrayNum[i]) {
-//				menor = arrayNum[i];
-//			}
-//		}
-//		System.out.println("El mayor número es: "+menor);
-//		System.out.println("--------------------------------------");
+		System.out.println("-----------------------------------------------------------------");
 	}
 	
 	/**
-	 * Mostrar la cantidad de números positivos introducidos
+	 * Método que calcula el menor número del array bidimensional
 	 */
-	public void positivNum() {
+	public void menorNum(int[][] arrayBiNum) {
+		int numMen = 999;
+		for(int i=0;i<arrayBiNum.length;i++) {
+			for(int j=0;j<arrayBiNum[i].length;j++) {
+				if(numMen > arrayBiNum[i][j]) {
+					numMen = arrayBiNum[i][j];
+				}
+
+			}	
+		}
+		System.out.println("El menor número es: "+numMen);
+	}
+	
+	/**
+	 * Mostrar la cantidad de números positivos introducidos del array unidimensional
+	 */
+	public void positivNum(int[] arrayUniNum) {
 		int cantPositiv = 0, cantNegativ = 0, cantCeros = 0;
 		Arrays.sort(arrayUniNum);
 		for(int i=0;i<arrayUniNum.length;i++) {
@@ -197,9 +260,32 @@ public class Number {
 		System.out.println("La cantidad de números positivos introducidos es: "+cantPositiv
 						+"\nLa cantidad de números negativos introducidos es: "+cantNegativ
 						+"\nLa cantidad de ceros introducidos es: "+cantCeros
-						+"\n--------------------------------------");
+						+"\n-----------------------------------------------------------------");
 	}
 	
-
+	/**
+	 * Mostrar la cantidad de números positivos introducidos del array bidimensional
+	 */
+	public void positivNum(int[][] arrayBiNum) {
+		int cantPositiv = 0, cantNegativ = 0, cantCeros = 0;
+		
+		for(int i=0;i<arrayBiNum.length;i++) {
+			for(int j=0;j<arrayBiNum[i].length;j++) {
+				if(arrayBiNum[i][j] > 0) {
+					cantPositiv++;
+				}
+				else if(arrayBiNum[i][j] < 0){
+					cantNegativ++;
+				}
+				else {
+					cantCeros++;
+				}
+			}
+		}
+		System.out.println("La cantidad de números positivos introducidos es: "+cantPositiv
+						+"\nLa cantidad de números negativos introducidos es: "+cantNegativ
+						+"\nLa cantidad de ceros introducidos es: "+cantCeros
+						+"\n-----------------------------------------------------------------");
+	}
 	
 }
