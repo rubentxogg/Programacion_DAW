50142e11420c1d2d1c15015c872f6aebec490c84
Rubén García <rubentxogg@hotmail.com>
2021-03-03 20:08:46 +0100

    [MOD] Práctica Imperativo vs Funcional, modo imperativo terminado

--- Enfoeque_imperativo_vs_funcional/src/ImperativeWay.java
+++ Enfoeque_imperativo_vs_funcional/src/ImperativeWay.java
@@ -2,32 +2,90 @@ import java.util.Arrays;
 
 public class ImperativeWay {
 	
+	/**
+	 * Método que lo que hará es recibir por parámetro un array de String y un string.
+	 * El método devolverá void y lo que hará es filtrar todos los elementos que del array que empiecen por el string que le hemos pasado por parámetro, 
+	 * luego los ordenará y luego los imprimirá. 
+	 * @param nombres
+	 * @param c
+	 */
 	public void filtraOrdenaImprime(String[] nombres, String c) {
-		String[] arrayAux = null;
-		int i = 0;
+		String[] arrayAux = null; // Array auxiliar
+		int cont = 0; // Contador
 		
 		for(String x : nombres) {
 			
 			if(x.startsWith(c)) {
-				i++;
+				cont++;
 			}
 		}
 		
-		arrayAux = new String[i];
-		i = 0;
+		arrayAux = new String[cont]; // Le ponemos la longitud del contador al array auxiliar
+		cont = 0; // Volvemos a poner el contador a 0;
 		
 		for(String x : nombres) {
 			
 			if(x.startsWith(c)) {
-				arrayAux[i] = x;
-				i++;
+				arrayAux[cont] = x;
+				cont++;
 			}
 		}
+		
+		Arrays.sort(arrayAux); // Ordenamos el array
+		
+		for(String x : arrayAux) { // Imprimimos el array
+			System.out.println(x);
+		}
+	}
+	
+	/**
+	 * El método recibirá un array de int, calculará los cuadrados de cada uno de los elementos y 
+	 * posteriormente calculará la media de esos números. 
+	 * @param numeros
+	 */
+	public void calculaCuadradoyMedia(int [] numeros) {
+		int media = 0;
+		
+		for(int x : numeros) {
+			
+			x = (int) Math.sqrt(x); // Hallo el cuadrado de cada elemento
+			media = media + x; // Sumo el cuadrado de cada elemento a media
+		}
+		
+		media = media / numeros.length; // Divido la suma de los elementos entre la longitud del array
+		System.out.println(media);	
+	}
+	
+	/**
+	 * Convierte a todos los strings del array a minúsculas, filtra los que empiecen con el String s que le pasamos por parámetro y los imprime. 
+	 * @param nombres
+	 * @param s
+	 */
+	public void convierteAMinusculasFiltra(String[] nombres, String s) {
+		String[] arrayAux = null; // Array auxiliar
+		int cont = 0;
+		
+		for(String x : nombres) {
+			if(x.startsWith(s)) {
+				cont++;
+			}
+		}
+		
+		arrayAux = new String[cont]; // Creo el array auxiliar con un máximo de elementos cont
+		cont = 0; // Contador a 0
+		
+		for(String x : nombres) {
+			if(x.startsWith(s)) {
+				arrayAux[cont] = x;
+				cont++;
+			}
+		}
+		
 		
 		for(String x : arrayAux) {
+			x = x.toLowerCase();
 			System.out.println(x);
 		}
 		
-		Arrays.sort(arrayAux);
 	}
 }
--- Enfoeque_imperativo_vs_funcional/src/ImperativoFuncional.java
+++ Enfoeque_imperativo_vs_funcional/src/ImperativoFuncional.java
@@ -2,8 +2,16 @@
 public class ImperativoFuncional {
 	
 	public static void main(String[] args) {
-		String[] arrayString = {"Paco", "Rubén", "Diego", "Amparo", "Rudy"}; 
+		String[] arrayString = {"Santiago", "Paco", "Ruperto", "Sara", "Rubén", "Diego", "Amparo", "Rudy", "Soraya"}; 
+		int[] arrayNumeros = {150, 225, 396, 587, 905, 1050};
 		
+		// Manera Imperativo
 		new ImperativeWay().filtraOrdenaImprime(arrayString, "Ru");
+		System.out.println();
+		new ImperativeWay().calculaCuadradoyMedia(arrayNumeros);
+		System.out.println();
+		new ImperativeWay().convierteAMinusculasFiltra(arrayString, "Sa");
+		
+		// Manera Funcional
 	}
 }
