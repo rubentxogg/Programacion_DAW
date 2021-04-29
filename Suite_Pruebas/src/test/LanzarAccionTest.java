e3d0c73bf0de389ce053326f982100d1cdd16bf0
Rubén García <rubentxogg@hotmail.com>
2021-04-29 18:58:55 +0200

    [ADD] jUnit5 Suite_Pruebas

--- Suite_Pruebas/src/test/LanzarAccionTest.java
+++ Suite_Pruebas/src/test/LanzarAccionTest.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import excepciones.MatrizException;
@@ -15,11 +16,20 @@ class LanzarAccionTest {
 	static LanzarAccion lanzarAccion;
 	static Scanner sc;
 	
+	// Objetos para probar excepciones
+	static LanzarAccion excRellenarMatriz;
+	static LanzarAccion excRecuperarValorEntrada;
+	
 	@BeforeAll
 	static void testLanzarAccion() {
 		lanzarAccion = new LanzarAccion();
 		lanzarAccion.inicializarAcciones();
 		sc = lanzarAccion.getScanner();
+		
+		excRellenarMatriz = new LanzarAccion();
+		excRellenarMatriz.inicializarAcciones();
+		excRecuperarValorEntrada = new LanzarAccion();
+		excRecuperarValorEntrada.inicializarAcciones();
 	}
 
 	@Test
@@ -28,17 +38,19 @@ class LanzarAccionTest {
 	}
 
 	@Test
+	@DisplayName("testRecuperarValorEntrada() -> Valor 5")
 	void testRecuperarValorEntrada() throws MatrizException {
-		assertEquals(5, lanzarAccion.recuperarValorEntrada()); // Introducir 5 como valor de entrada
+		assertEquals(5, lanzarAccion.recuperarValorEntrada());
 	}
 
 	@Test
+	@DisplayName("excepcionRecuperarValorEntrada() -> Un carácter")
 	void excepcionRecuperarValorEntrada() {
-		Exception exc = assertThrows(MatrizException.class, () -> lanzarAccion.recuperarValorEntrada()); // Introducir un carácter
+		Exception exc = assertThrows(MatrizException.class, () -> excRecuperarValorEntrada.recuperarValorEntrada());
 		assertEquals("El valor introducido no es del tipo correcto", exc.getMessage());
 	}
 	
-	@AfterAll
+	@AfterAll 
 	static void testTerminar() {
 		lanzarAccion.terminar();
 	}
@@ -49,30 +61,38 @@ class LanzarAccionTest {
 	}
 	
 	@Test
+	@DisplayName("excepcionRellenarMatriz() -> Un carácter")
 	void excepcionRellenarMatriz() {
-		Exception exc = assertThrows(MatrizException.class, () -> lanzarAccion.recuperarValorEntrada());
-		assertEquals(exc.getMessage()+" la matriz no se ha rellenado.", lanzarAccion.rellenarMatriz()); // Introducir un carácter
+		Exception exc = assertThrows(MatrizException.class, () -> excRellenarMatriz.recuperarValorEntrada());
+		assertEquals(exc.getMessage()+" la matriz no se ha rellenado.", excRellenarMatriz.rellenarMatriz());
 	}
 
-
 	@Test
+	@DisplayName("testSumarDatosFila() -> Valor 2 en la fila 4")
 	void testSumarDatosFila() {
-		fail("Not yet implemented");
+		lanzarAccion.rellenarMatriz();
+		assertEquals("La suma de los valores de la fila " + 4 + " es: " + 8, lanzarAccion.sumarDatosFila());
 	}
 
 	@Test
+	@DisplayName("testSumarDatosColumna() -> Valor 1 en la columna 1")
 	void testSumarDatosColumna() {
-		fail("Not yet implemented");
+		lanzarAccion.rellenarMatriz(); // Introducir el valor 1 en la columna uno
+		assertEquals("La suma de los valores de la columna " + 1 + " es: " + 4, lanzarAccion.sumarDatosColumna());
 	}
 
 	@Test
+	@DisplayName("testCalcularMediaValores() -> Solo valor 5")
 	void testCalcularMediaValores() {
-		fail("Not yet implemented");
+		lanzarAccion.rellenarMatriz(); // Introducir solamente el 5 como valor
+		assertEquals("La media de los valores de la " + "matriz es de " + 5.0, lanzarAccion.calcularMediaValores());
 	}
 
 	@Test
+	@DisplayName("testRecuperarCeldaMatriz() -> Valor 9 en fila 2, columna 3")
 	void testRecuperarCeldaMatriz() {
-		fail("Not yet implemented");
+		lanzarAccion.rellenarMatriz(); // Introducir el valor 9 en la fila dos, columna tres
+		assertEquals("El valor almacenado en la fila " + 2 + " columna " + 3 + "es " + 9, lanzarAccion.recuperarCeldaMatriz());
 	}
 
 }
