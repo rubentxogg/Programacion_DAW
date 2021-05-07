9f8c25443e4d9274e4581ddac7aa8393d1cfdcfc
Rubén García <rubentxogg@hotmail.com>
2021-05-07 17:19:58 +0200

    [MOD] Práctica ayt. Salamanca -> Lista de mascotas arreglada

--- Práctica_Ayt_Salamanca/src/com/rgg/dto/Mascota.java
+++ Práctica_Ayt_Salamanca/src/com/rgg/dto/Mascota.java
@@ -12,6 +12,8 @@ import java.util.Scanner;
 public class Mascota {
 
 	private List<MascotaDTO> listaMascotas = new ArrayList<>();
+	private List<MascotaDTO> listaPerros = new ArrayList<>();
+	private List<MascotaDTO> listaGatos = new ArrayList<>();
 	private Scanner sc = new Scanner(System.in);
 	private int cont = 1;
 	
@@ -21,9 +23,9 @@ public class Mascota {
 	public void solicitarDatosMascota() {
 		boolean continuar = true;
 		String respuesta;
-		MascotaDTO mascota = new MascotaDTO();
 		
 		while(continuar) {
+			MascotaDTO mascota = new MascotaDTO();
 			System.out.print("Introduzca el nombre de la mascota: ");
 			mascota.setNombre(sc.nextLine());
 			System.out.print("Introduzca la edad: ");
@@ -33,7 +35,7 @@ public class Mascota {
 			mascota.setEspecie(sc.nextLine());
 			
 			listaMascotas.add(mascota);
-			System.out.println("\nMascota añadida, ¿Desea continuar? s/n");
+			System.out.print("\nMascota añadida, ¿Desea continuar? s/n: ");
 			respuesta = sc.next();
 			sc.nextLine();
 			continuar = (respuesta.equalsIgnoreCase("s"))?continuar:!continuar;
@@ -48,18 +50,33 @@ public class Mascota {
 	}
 	
 	/**
+	 * Retira de la lista de mascotas a los perros y los gatos y los introduce en su lista correspondiente
+	 */
+	// TODO
+	public void clasificarMascotas() {
+		Iterator<MascotaDTO> iterator = listaMascotas.iterator();
+		
+		while(iterator.hasNext()) {
+			MascotaDTO mascota = iterator.next();
+			switch(mascota.getEspecie().toLowerCase()) {
+				case "perro":
+			}
+		}
+	}
+	
+	/**
 	 * Muestra los datos guardados en la lista de mascotas
 	 */
 	public void mostrarListaMascotas() {
-		Iterator<MascotaDTO> i = listaMascotas.iterator();
+		Iterator<MascotaDTO> iterator = listaMascotas.iterator();
 		
 		System.out.println("\n-=Lista de mascotas=-\n");
-		while(i.hasNext()) {
-			MascotaDTO m = i.next();
+		while(iterator.hasNext()) {
+			MascotaDTO mascota = iterator.next();
 			System.out.println("#"+cont);
-			System.out.println("Nombre: "+m.getNombre());
-			System.out.println("Edad: "+m.getEdad());
-			System.out.println("Especie: "+m.getEspecie()+"\n");
+			System.out.println("Nombre: "+mascota.getNombre());
+			System.out.println("Edad: "+mascota.getEdad());
+			System.out.println("Especie: "+mascota.getEspecie()+"\n");
 			cont++;
 		}
 	}
